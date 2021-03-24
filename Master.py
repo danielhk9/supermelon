e7cffe8bb8d3d6478fa2d2ba826c8ecaafa6ce42
danielhk9 <bethegit5437>
2021-03-24 18:31:55 +0200

    change the test name

--- Master.py
+++ Master.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import unittest2
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
@@ -9,23 +11,17 @@ from BeforeSignIn.ReadyToShip import CheckReadyToShip
 from BeforeSignIn.AboutUS import CheckAboutUs
 from Helpers.Functions import changeWindowAndSwitch, findElementsByXpath
 from SignInAndOut.LoginToSite import LoginFeature
-from AfterSignIn.UrlAndProducts import CheckAllProducts
+#from AfterSignIn.UrlAndProducts import CheckAllProducts
 import os
-from pyvirtualdisplay import Display
 
 class InitFlow(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
         print("setUpClass")
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--window-size=1420,1080')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
         capabilities = DesiredCapabilities.CHROME
         capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
-        cls.driver = webdriver.Chrome(executable_path=f'{os.getcwd()}/chromedriver', desired_capabilities=capabilities,chrome_options=chrome_options)
+        cls.driver = webdriver.Chrome(executable_path=f'{os.getcwd()}/chromedriver', desired_capabilities=capabilities, options=options)
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
         cls.driver.get("https://supermelon.com/")
