2dafc41783a521a736c207653893f72ed20b5cdc
danielhk9 <bethegit5437>
2021-04-05 01:23:08 +0300

    change the test name

--- AWS/UploadImage.py
+++ AWS/UploadImage.py
@@ -23,7 +23,7 @@ def imageToAWS():
     with open(f'{os.getcwd()}/Files/awsKeys.csv') as code:
         keys = code.readline().split(",")
     clientS3 = boto3.client('s3', aws_access_key_id=keys[0], aws_secret_access_key=keys[1])
-    clientRekognition = boto3.client('rekognition', aws_access_key_id=keys[0], aws_secret_access_key=keys[1])
+    clientRekognition = boto3.client('rekognition',  region_name='us-east-2', aws_access_key_id=keys[0], aws_secret_access_key=keys[1])
     images = getAllImages()
     try:
         for productID, allImages in images.items():
