f5e697974a4d651fe69b30c071ecc08d5d092cb7
danielhk9 <bethegit5437>
2021-04-05 03:01:31 +0300

    change the test name

--- AWS/UploadImage.py
+++ AWS/UploadImage.py
@@ -48,7 +48,7 @@ def imageToAWS():
                 if size:
                     startNumOfSize = writeToExecl(sizeWs, size, imageName, productID, imageURL, startNumOfSize)
                 deleteTheImage(imagePath)
-                if s == 1000:
+                if s == 2:
                     break
 
     except Exception as e:
